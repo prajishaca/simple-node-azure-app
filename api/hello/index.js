be3404d214c3835f73eeda5c54c434f783a2d937
prajisha <c_prajisha@cb.amrita.edu>
2025-07-16 14:02:59 +0530

    Updated project with new changes

--- api/hello/index.js
+++ api/hello/index.js
@@ -1,7 +1,7 @@
-// api/hello.js
-
+// api/hello/index.js
 module.exports = async function (context, req) {
   context.res = {
+    status: 200,
     headers: { "Content-Type": "application/json" },
     body: { message: "Hello from Azure API!" },
   };
